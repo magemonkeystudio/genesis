b5f5884dd361f8277af08bc39b50913f25519f30
lijinhong11 <mcjrhugh@163.com>
2022-12-04 08:39:04 +0800

    Rename method

--- src/main/java/org/black_ixx/bossshop/core/conditions/BSConditionTypeClientVersion.java
+++ src/main/java/org/black_ixx/bossshop/core/conditions/BSConditionTypeClientVersion.java
@@ -1,14 +1,9 @@
 package org.black_ixx.bossshop.core.conditions;
 
-import net.minecraft.server.v1_13_R2.EntityPlayer;
-import net.minecraft.server.v1_13_R2.NetworkManager;
-import net.minecraft.server.v1_13_R2.PlayerConnection;
 import org.black_ixx.bossshop.core.BSBuy;
 import org.black_ixx.bossshop.core.BSShopHolder;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
-import java.lang.reflect.Field;
 
 public class BSConditionTypeClientVersion extends BSConditionTypeNumber{
     @Override
@@ -29,13 +24,16 @@ public class BSConditionTypeClientVersion extends BSConditionTypeNumber{
         return getPlayerProtocolVersion(p);
     }
 
-    private static int getPlayerProtocolVersion(Player player) throws NoSuchFieldException, IllegalAccessException {
-        CraftPlayer craftPlayer = (CraftPlayer) player;
-        EntityPlayer entityPlayer = craftPlayer.getHandle();
-        PlayerConnection connection = entityPlayer.playerConnection;
-        NetworkManager networkManager = connection.networkManager;
-        Field protocolVersionField = networkManager.getClass().getDeclaredField("protocolVersion");
-        protocolVersionField.setAccessible(true);
-        return protocolVersionField.getInt(networkManager);
+    private static int getPlayerProtocolVersion(Player player) {
+        String version = Bukkit.getServer().getBukkitVersion().split("-")[0];
+        String[] vers = version.split("\\.");
+        Class c;
+        try { c = Class.forName("net.minecraft.server.v"+vers[0]+"_"+vers[1]+"_R"+vers[2]+".entity.CraftPlayer");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+            return 0;
+        }
+        //I can't use NMS...
+        return 0;
     }
 }
