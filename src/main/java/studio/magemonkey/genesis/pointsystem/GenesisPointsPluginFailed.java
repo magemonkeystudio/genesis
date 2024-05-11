1ee9a0c100668c513c7bf6590612590bf8b907a7
Travja <the.only.t.craft@gmail.com>
2024-05-11 17:02:00 -0600

    Clean up player points spam

--- src/main/java/studio/magemonkey/genesis/pointsystem/GenesisPointsPluginFailed.java
+++ src/main/java/studio/magemonkey/genesis/pointsystem/GenesisPointsPluginFailed.java
@@ -3,7 +3,13 @@ package studio.magemonkey.genesis.pointsystem;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class GenesisPointsPluginFailed extends GenesisPointsPlugin {
+    private       boolean   notifiedConsole = false;
+    private final Set<UUID> notifiedPlayers = new HashSet<>();
 
     public GenesisPointsPluginFailed() {
         super("Failed");
@@ -30,11 +36,18 @@ public class GenesisPointsPluginFailed extends GenesisPointsPlugin {
     }
 
     private void informPlayer(OfflinePlayer player) { //Not sending to prevent spam on servers that installed Genesis for the first time. It should be enough when Genesis notifies about this on startup.
-        Bukkit.getConsoleSender()
-                .sendMessage("PlayerPoints/CommandPoints was not found... "
-                        + "You need one of that plugins if you want to work with Points! " + "Get PlayerPoints there: "
-                        + "http://dev.bukkit.org/server-mods/playerpoints/");
-        if (player.isOnline())
+        if (!notifiedConsole) {
+            notifiedConsole = true;
+            Bukkit.getConsoleSender()
+                    .sendMessage("PlayerPoints/CommandPoints was not found... "
+                            + "You need one of that plugins if you want to work with Points! "
+                            + "Get PlayerPoints there: "
+                            + "https://dev.bukkit.org/server-mods/playerpoints/");
+        }
+
+        if (notifiedPlayers.contains(player.getUniqueId())) return;
+
+        if (player.isOnline()) {
             if (player.isOp()) {
                 player.getPlayer()
                         .sendMessage(
@@ -43,7 +56,8 @@ public class GenesisPointsPluginFailed extends GenesisPointsPlugin {
                 player.getPlayer()
                         .sendMessage("[Genesis] No Points Plugin installed. Please inform an administrator.");
             }
-
+            notifiedPlayers.add(player.getUniqueId());
+        }
     }
 
     @Override
