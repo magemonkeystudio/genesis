c61ee87cef3f52fd789511ddbbd179562e09fda9
Travja <the.only.t.craft@gmail.com>
2024-08-03 16:54:06 -0600

    Move folia bridge to separate module

--- src/main/java/org/black_ixx/bossshoppro/folia/CrossScheduler.java
+++ src/main/java/org/black_ixx/bossshoppro/folia/CrossScheduler.java
@@ -1,10 +1,10 @@
 package org.black_ixx.bossshoppro.folia;
 
-import com.promcteam.profoliabridge.ProFoliaBridge;
 import org.black_ixx.bossshop.BossShop;
 import org.black_ixx.bossshop.managers.ClassManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import studio.magemonkey.foliabridge.FoliaBridge;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
@@ -12,63 +12,63 @@ import java.util.concurrent.Future;
 
 public class CrossScheduler {
 
-    private static final BossShop plugin = ClassManager.manager.getPlugin();
-    private static final boolean isFolia = Bukkit.getVersion().contains("Folia");
+    private static final BossShop plugin  = ClassManager.manager.getPlugin();
+    private static final boolean  isFolia = Bukkit.getVersion().contains("Folia");
 
     public static boolean isFolia() {
         return isFolia;
     }
 
     public static void run(Runnable runnable) {
-        if(isFolia) {
-            ProFoliaBridge.execute(plugin, runnable);
+        if (isFolia) {
+            FoliaBridge.execute(plugin, runnable);
         } else {
             Bukkit.getScheduler().runTask(plugin, runnable);
         }
     }
 
     public static void runAsync(Runnable runnable) {
-        if(isFolia) {
-            ProFoliaBridge.execute(plugin, runnable);
+        if (isFolia) {
+            FoliaBridge.execute(plugin, runnable);
         } else {
             Bukkit.getScheduler().runTaskAsynchronously(plugin, runnable);
         }
     }
 
     public static void runTaskLater(Runnable runnable, long delayTicks) {
-        if(isFolia) {
-            ProFoliaBridge.runDelayed(plugin, runnable, delayTicks);
+        if (isFolia) {
+            FoliaBridge.runDelayed(plugin, runnable, delayTicks);
         } else {
             Bukkit.getScheduler().runTaskLater(plugin, runnable, delayTicks);
         }
     }
 
     public static void runTaskLaterAsync(Runnable runnable, long delay) {
-        if(isFolia) {
-            ProFoliaBridge.runDelayed(plugin, runnable, delay);
+        if (isFolia) {
+            FoliaBridge.runDelayed(plugin, runnable, delay);
         } else {
             Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, runnable, delay);
         }
     }
 
     public static void runTaskTimer(Runnable runnable, long delay, long period) {
-        if(isFolia) {
-            ProFoliaBridge.runAtFixedRate(plugin, runnable, delay < 1 ? 1 : delay, period);
+        if (isFolia) {
+            FoliaBridge.runAtFixedRate(plugin, runnable, delay < 1 ? 1 : delay, period);
         } else {
             Bukkit.getScheduler().runTaskTimer(plugin, runnable, delay, period);
         }
     }
 
     public static void runTaskTimerAsync(Runnable runnable, long delay, long period) {
-        if(isFolia) {
-            ProFoliaBridge.runAtFixedRate(plugin, runnable, delay < 1 ? 1 : delay, period);
+        if (isFolia) {
+            FoliaBridge.runAtFixedRate(plugin, runnable, delay < 1 ? 1 : delay, period);
         } else {
             Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, runnable, delay, period);
         }
     }
 
     public static <T> Future<T> callSyncMethod(Callable<T> task) {
-        if(isFolia) {
+        if (isFolia) {
             ClassManager.manager.getBugFinder().warn("Folia does not support cancelling tasks by ID");
             runTaskLater(() -> {
                 try {
@@ -82,8 +82,9 @@ public class CrossScheduler {
             return Bukkit.getScheduler().callSyncMethod(plugin, task);
         }
     }
+
     public static void cancelTask(int taskId) {
-        if(isFolia) {
+        if (isFolia) {
             ClassManager.manager.getBugFinder().warn("Folia does not support cancelling tasks by ID");
         } else {
             Bukkit.getScheduler().cancelTask(taskId);
@@ -91,8 +92,8 @@ public class CrossScheduler {
     }
 
     public static void cancelTasks(Plugin plugin) {
-        if(isFolia) {
-            ProFoliaBridge.cancelTasks(CrossScheduler.plugin, plugin);
+        if (isFolia) {
+            FoliaBridge.cancelTasks(CrossScheduler.plugin, plugin);
         } else {
             Bukkit.getScheduler().cancelTasks(plugin);
         }
